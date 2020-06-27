d19a649abfa63a45f5e0ad87860b3c442ef5395a
Aritra Bhattacharjee <whoisaritra@gmail.com>
2020-06-27 22:02:00 +0530

    Added Animation to Auth Page

--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -11,10 +11,10 @@ class LandingPage extends StatelessWidget {
             PageView(
               controller: _pageController,
               children: <Widget>[
-                Container(color: Colors.red,),
-                Container(color: Colors.green,),
-                Container(color: Colors.yellow,),
-                Container(color: Colors.blue,),
+                Page(color: Colors.red, text: "Page 1",),
+                Page(color: Colors.green, text: "Page 2"),
+                Page(color: Colors.yellow, text: "Page 3"),
+                Page(color: Colors.blue, text: "Page 4"),
               ],
             ),
             Positioned(
@@ -28,7 +28,6 @@ class LandingPage extends StatelessWidget {
 
                     }, child: Text("Login"),),
                     RaisedButton(onPressed: (){}, child: Text("Register"),),
-
                   ],
                 ),
               ),
@@ -36,6 +35,20 @@ class LandingPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+
+class Page extends StatelessWidget {
+  final Color color;
+  final String text;
+  Page({Key key, this.color, this.text}) : super(key : key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: Center(child: Text(text)),
     );
   }
 }
--- lib/pages/Wrapper.dart
+++ lib/pages/Wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_robin_app/pages/LandingPage.dart';
 import 'package:the_robin_app/widgets/AuthForm.dart';
 
 
@@ -6,5 +7,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AuthForm(signup: true,);
+//  return LandingPage();
   }
 }

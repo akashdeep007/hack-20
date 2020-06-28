b99c6694b70913a8b787b57d98fcddfc65a8c1a5
Aritra Bhattacharjee <whoisaritra@gmail.com>
2020-06-28 08:39:54 +0530

    Added Loading

--- lib/widgets/Login.dart
+++ lib/widgets/Login.dart
@@ -44,81 +44,89 @@ class _LoginState extends State<Login> {
     final AuthService _auth = AuthService();
     final width = MediaQuery.of(context).size.width;
 //    final height = MediaQuery.of(context).size.height;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    print(isLoading);
+    return Stack(
       children: <Widget>[
-        SizedBox(
-          height: 1,
-        ),
         Container(
-          margin: EdgeInsets.only(left: 10),
-          width: width,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text(
-                "Welcome",
-                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 1,
               ),
-              Text("Sign in to your Account"),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Welcome",
+                      style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Sign in to your Account"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical : 20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+//              _buildFormField("Email", inputHandler),
+//              _buildFormField("Password", inputHandler)
+                      CustomInputField(title: "Email", icon: Icons.email, error: _emailError, inputHandler: inputHandler,),
+                      CustomInputField(title: "Password", icon: Icons.lock, error: _passwordError, inputHandler: inputHandler, isObsecure: true,),
+
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () async {
+                      print(_email + " " + _password);
+                      if(!validators.isEmail(_email.trim())) setState(() => _emailError = true);
+                      if(_password.trim().length < 8) setState(() => _passwordError = true);
+                      if(!_emailError && !_passwordError) {
+                        setState(() => isLoading = true);
+                        await _auth.signInWithEmailAndPassword(_email, _password);
+                        setState(() => isLoading = false);
+                      };
+                    },
+                    child: Text("Sign In"),
+                    elevation: 3,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Do not have an Account ?",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      InkWell(
+                          onTap: () => widget.changeForm(),
+                          child: Text(
+                            "Register",
+                            style: TextStyle(fontSize: 14, color: Colors.green),
+                          ))
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical : 20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-//              _buildFormField("Email", inputHandler),
-//              _buildFormField("Password", inputHandler)
-                CustomInputField(title: "Email", icon: Icons.email, error: _emailError, inputHandler: inputHandler,),
-                CustomInputField(title: "Password", icon: Icons.lock, error: _passwordError, inputHandler: inputHandler, isObsecure: true,),
-
-              ],
-            ),
-          ),
-        ),
-        Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () async {
-                print(_email + " " + _password);
-                if(!validators.isEmail(_email.trim())) setState(() => _emailError = true);
-                if(_password.trim().length < 8) setState(() => _passwordError = true);
-                if(!_emailError && !_passwordError) {
-                  setState(() => isLoading = true);
-                  _auth.signInWithEmailAndPassword(_email, _password);
-                  setState(() => isLoading = false);
-                };
-                },
-              child: Text("Sign In"),
-              elevation: 3,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Do not have an Account ?",
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                InkWell(
-                    onTap: () => widget.changeForm(),
-                    child: Text(
-                      "Register",
-                      style: TextStyle(fontSize: 14, color: Colors.green),
-                    ))
-              ],
-            ),
-          ],
-        ),
+        isLoading ? Center(child: CircularProgressIndicator(),) : Container(),
       ],
     );
   }
--- lib/widgets/Signup.dart
+++ lib/widgets/Signup.dart
@@ -58,78 +58,85 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 //    final height = MediaQuery.of(context).size.height;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Stack(
       children: <Widget>[
         Container(
-          margin: EdgeInsets.only(left: 10),
-          width: width,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Register",
-                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Form(
-          key: _formKey,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              CustomInputField(icon: Icons.person, inputHandler: inputHandler, error: _nameError, title: "Name"),
-              CustomInputField(icon: Icons.email, inputHandler: inputHandler, error: _emailError, title: "Email"),
-              CustomInputField(icon: Icons.phone, inputHandler: inputHandler, error: _phoneError, title: "Phone"),
-              CustomInputField(icon: Icons.lock, inputHandler: inputHandler, error: _passwordError, title: "Password", isObsecure: true,),
-              CustomInputField(icon: Icons.lock, inputHandler: inputHandler, error: _confirmError, title: "Confirm Password", isObsecure: true,),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: width,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Register",
+                        style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    CustomInputField(icon: Icons.person, inputHandler: inputHandler, error: _nameError, title: "Name"),
+                    CustomInputField(icon: Icons.email, inputHandler: inputHandler, error: _emailError, title: "Email"),
+                    CustomInputField(icon: Icons.phone, inputHandler: inputHandler, error: _phoneError, title: "Phone"),
+                    CustomInputField(icon: Icons.lock, inputHandler: inputHandler, error: _passwordError, title: "Password", isObsecure: true,),
+                    CustomInputField(icon: Icons.lock, inputHandler: inputHandler, error: _confirmError, title: "Confirm Password", isObsecure: true,),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              Column(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("Register"),
+                    onPressed: () async {
+                      if(_name.length <= 0) setState(() => _nameError = true);
+                      if(!validators.isEmail(_email.trim())) setState(() => _emailError = true);
+                      if(!validators.isNumeric(_phone.trim()) || _phone.trim().length != 10) setState(() => _phoneError = true);
+                      if(_password.trim().length <= 8 ) setState(() => _passwordError = true);
+                      if(_password.trim() != _confirm.trim()) setState(() => _confirmError = true);
+                      if(_confirm.trim().length <= 8) setState(() => _confirmError = true);
+                      if(!_nameError && !_confirmError && !_phoneError && !_passwordError && !_emailError){
+                        setState(() => isLoading = true);
+                        await _auth.registerWithEmailAndPassword(_name, _email, _phone, _password);
+                        setState(() => isLoading = false);
+
+                      }
+                    },
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Already Have an Account?",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      InkWell(
+                          onTap: () => widget.changeForm(),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 14, color: Colors.green),
+                          ))
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
-        SizedBox(height: 10,),
-        Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text("Register"),
-              onPressed: () async {
-                if(_name.length <= 0) setState(() => _nameError = true);
-                if(!validators.isEmail(_email.trim())) setState(() => _emailError = true);
-                if(!validators.isNumeric(_phone.trim()) && _phone.trim().length != 10) setState(() => _phoneError = true);
-                if(_password.trim().length <= 8 ) setState(() => _passwordError = true);
-                if(_password.trim() != _confirm.trim()) setState(() => _confirmError = true);
-                if(_confirm.trim().length <= 8) setState(() => _confirmError = true);
-                if(!_nameError && !_confirmError && !_phoneError && !_passwordError && !_emailError){
-                  setState(() => isLoading = true);
-                  await _auth.registerWithEmailAndPassword(_name, _email, _phone, _password);
-                  setState(() => isLoading = false);
-
-                }
-              },
-            ),
-            SizedBox(height: 5,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Already Have an Account?",
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                InkWell(
-                    onTap: () => widget.changeForm(),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 14, color: Colors.green),
-                    ))
-              ],
-            ),
-          ],
-        ),
+        isLoading ? Center(child: CircularProgressIndicator(),) : Container()
       ],
     );
   }

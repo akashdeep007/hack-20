896a5ca4e936e81c222cdc49c92a2278c0df2146
Aritra Bhattacharjee <whoisaritra@gmail.com>
2020-06-28 21:26:13 +0530

    Added Profile Page

--- lib/pages/Main/HomePage.dart
+++ lib/pages/Main/HomePage.dart
@@ -22,7 +22,11 @@ class _HomePageState extends State<HomePage> {
       Map<String, String> events = {
         "event": event,
         "location": location,
-        "date": date.toString()
+        "date": date.toString()+
+
+
+
+      
       };
 
       documentReference.setData(events).whenComplete(() {
--- lib/pages/Main/Profile.dart
+++ lib/pages/Main/Profile.dart
@@ -1,25 +1,62 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:the_robin_app/blocs/AuthService.dart';
 import 'package:the_robin_app/models/User.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+
     final AuthService _auth = AuthService();
     final user = Provider.of<User>(context);
+    final height = MediaQuery.of(context).size.height;
+
+    Widget _buildInfoTile(String title, double width){
+      return Container(
+        width: width,
+        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Material(
+            elevation: 3,
+            borderRadius: BorderRadius.circular(29),
+            shadowColor: Colors.grey,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Text(title, textAlign: TextAlign.center,),
+            )),
+      );
+    }
+    double maxContWidth = 0;
+    if(user != null) maxContWidth = ((user.email.length+5) * 10).toDouble();
     return user != null ? SizedBox.expand(
       child: Stack(
-        alignment: Alignment.center,
+        alignment: Alignment.topCenter,
         children: <Widget>[
-          CustomPaint(painter: BackDropPaint(),child: Container(),),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(user.name),
-              RaisedButton(onPressed: _auth.signOut, child: Text("Log Out"),)
-            ],
+          CustomPaint(painter: BackDropPaint(),child: Container(height: height / 2.5 ,alignment: Alignment.bottomCenter,child: Material(borderRadius: BorderRadius.circular(60), shadowColor: Colors.grey,elevation: 6,child: ClipOval(child : Container(height: 120, width: 120,color: Colors.green,child: Placeholder()))),),),
+          Positioned(
+            top: height / 2.5,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(height: 10,),
+                Column(
+                  children: <Widget>[
+                    Text(user.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 10,),
+                    _buildInfoTile('Email : ${user.email}', maxContWidth),
+                    _buildInfoTile('Contact : ${user.phone}', maxContWidth),
+                  ],
+                ),
+                SizedBox(height: 50,),
+                RaisedButton(onPressed: _auth.signOut, child: Text("Log Out"), elevation: 6,)
+              ],
+            ),
           ),
 
         ],
@@ -32,8 +69,8 @@ class Profile extends StatelessWidget {
 class BackDropPaint extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
-    final center = Offset(size.width/2, 0);
-    canvas.drawCircle(center, 200, Paint()..color = Colors.greenAccent);
+    final center = Offset(size.width/2, -120);
+    canvas.drawCircle(center, 300, Paint()..color = Colors.greenAccent);
   }
 
   @override
